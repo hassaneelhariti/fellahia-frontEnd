092f5edb15b4a8b69be8c91ec6240243ac7c3656
Hassane El Hariti <hassaneelhariti@gmail.com>
2026-03-13 22:54:50 +0000

    feat(app): configure routes and application providers for fellah feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import {authGuard, roleGuard} from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -6,6 +7,14 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
   },
-
-
+  {
+    path: 'fellah',
+    canActivate: [authGuard, roleGuard('FELLAH')],
+    loadChildren: () => import('./features/fellah/fellah.routes').then(m => m.FELLAH_ROUTES)
+  },
+  // {
+  //   path: 'lawyer',
+  //   canActivate: [authGuard, roleGuard('AVOCAT')],
+  //   loadChildren: () => import('./features/lawyer/lawyer.routes').then(m => m.LAWYER_ROUTES)
+  // }
 ];
